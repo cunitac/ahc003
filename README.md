cc046cfc0d3406726e8c6c46d9637db6f2cf74cc
cunitac <cunitac@gmail.com>
2021-05-22 14:41:50 +0900

    初提出 666,233,374

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [AtCoder Heuristic Contest 003](https://atcoder.jp/contests/ahc003)の解答置き場。実際には、[cargo-equip](https://github.com/qryxip/cargo-equip)を使用して1ファイルにまとめて提出する。使用時にオプションを大量に指定するので、[equip](equip)にまとめている。
 
 自作ライブラリとして、[procon-rs-lib](https://github.com/cunitac/procon-rs-lib)を使用している。
+
+コミットに書いてあるのは[test](test)によるスコアで、提出ではない。
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[macro_use]
+extern crate input as _;
+
+const N_QUERY: usize = 1000;
+
 fn main() {
-    todo!()
+    for _ in 0..N_QUERY {
+        input!(start: (usize, usize), goal: (usize, usize));
+
+        print!(
+            "{}",
+            if goal.0 < start.0 {
+                "U".repeat(start.0 - goal.0)
+            } else {
+                "D".repeat(goal.0 - start.0)
+            }
+        );
+        println!(
+            "{}",
+            if goal.1 < start.1 {
+                "L".repeat(start.1 - goal.1)
+            } else {
+                "R".repeat(goal.1 - start.1)
+            }
+        );
+
+        input!(_path_len: u64);
+    }
 }
